633ba639fd20e921c48ff18b1a9c64e77e498eea
renxi <retixi@163.com>
2016-11-01 16:32:10 +0800

    添加登陆

--- app.js
+++ app.js
@@ -1,38 +1,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongojs=require('mongojs');
-var db=mongojs('xiaozhu',['fangzi']);
+var db = mongojs('local', ['users']);
+console.log('connetion');
 //var multipart = require('connect-multiparty');
 //var multipartMiddleware = multipart();
-
-
 //var morgan = require('morgan');
 var app = express();
 var server = require('http').createServer(app);
+app.set('views','./views');
+app.set('view engine','ejs');
 app.use(bodyParser.json({limit: '1mb'}));  //这里指定参数使用 json 格式
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 app.get('/users',function (req,res) {
-    res.send(`
-<form method="post">
-    name:<br>
-    <input type="text" name="name">
-    <br>
-    password:<br>
-    <input type="text" name="password">
-    uid:<br>
-    <input type="text" name="uid">
-    phone:<br>
-    <input type="text" name="phone">
-    <br><br>
-    <input type="submit" value="Submit">
-</form>
-`);
+    res.render('users.ejs');
 })
 
-
-
+//用户注册
 app.post('/users', function (req, res) {
     var User=req.body;
     var uid = req.body.uid;
@@ -44,18 +31,130 @@ app.post('/users', function (req, res) {
         });
     }
     else{
-    db.fangzi.findOne({uid: uid}, function (err, existUser){
+    db.users.findOne({uid: uid}, function (err, existUser){
         if(existUser){res.send('用户已经存在')}
-        else{db.fangzi.save({
+
+        else{db.users.save({
             uid:User.uid,
             name:User.name,
             password:User.password,
-            phone:User.phone
+            gender:User.gender,
+            birthday:User.birthday
 
         })
 
-            res.send('用户注册成功')}
+            res.send({"user":{
+                'uid':User.uid,
+                'name':User.name,
+                'password':User.password,
+                'gender':User.gender,
+                'birthday':User.birthday
+
+            },
+            "accesstoken":"dsfdsfsdafsda"
+            })}
     }
     )}
 })
-    server.listen(3000);
+
+//用户登录
+app.post('/login',function (req,res) {
+    var inputpassword=req.body.password;
+    var User=db.users.findOne({uid:req.body.uid},function (err,existuser) {
+        if(!existuser){res.send('用户不存在')}
+            if (existuser.password == inputpassword) {
+                res.send({
+                    "user": {
+                        "_id" : "57fc366c9a04201a98836126",
+                        "uid": "zhz821",
+                        "name": "張志華",
+                        "createdAt": "2016-12-01T08:40:51.620Z",
+                        "gender": 0,
+                        "birthday": "1982/01/16",
+                        "avatar": "xxxx.png",
+                        "inviting": [
+                            {"uid": "abc"},
+                            {"uid": "def"}
+                        ]
+                    },
+                    "accessToken": "abcdefghijklmn"
+                })
+            }
+
+    });
+});
+
+//取得自己信息
+app.get('/user',function (req,res) {
+    if (req.headers.authorization=='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9')
+    {       res.setHeader("Content-Type", "application/json")
+        res.send({
+            "_id" : "57fc366c9a04201a98836126",
+            "uid": "zhz821",
+            "name": "張志華",
+            "createdAt": "2016-12-01T08:40:51.620Z",
+            "gender": 0,
+            "birthday": "1982/01/16",
+            "avatar": "xxxx.png",
+            "inviting": [
+                {"uid": "abc"},
+                {"uid": "def"}
+            ],
+            "invited": [
+                {"uid": "ghi"},
+                {"uid": "jkl"}
+            ],
+            "friend": [
+                {"uid": "aaa"}
+            ]
+        })
+
+    }
+})
+
+//事件
+app.get('/events',function (req,res) {
+    if (req.headers.authorization=='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9')
+    {   res.setHeader("Content-Type", "application/json");
+        res.json(
+            [
+                {
+                    "_id": "5726ac024ff978af01400b57",
+                    "type": "inviting",
+                    "from": {
+                        "uid": "test04",
+                        "name": "Test04",
+                        "avatar": "xxxx.png"
+                    },
+                    "to": {
+                        "uid": "test05",
+                        "name": "Test05",
+                        "avatar": "xxxx.png"
+                    },
+                    "createdAt": "2016-12-01T08:40:51.620Z"
+                },
+                {
+                    "_id": "5726ac024ff978af01400b57",
+                    "type": "message",
+                    "from": {
+                        "uid": "test04",
+                        "name": "Test04",
+                        "avatar": "xxxx.png"
+                    },
+                    "to": {
+                        "uid": "test05",
+                        "name": "Test05",
+                        "avatar": "xxxx.png"
+                    },
+                    "event": "圣诞节",
+                    "mid": "m12345678",
+                    "createdAt": "2016-12-01T08:40:51.620Z"
+                }
+            ]
+            )
+
+    }
+})
+
+
+server.listen(3000);
